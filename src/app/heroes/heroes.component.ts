672562374b089175c9ec3270da151d5818bd3dde
Shelton Suen <sheltonsuen@gmail.com>
2019-01-04 09:34:36 +0800

    Add hero property.

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -6,10 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
+  hero: string = 'Shelton';
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }

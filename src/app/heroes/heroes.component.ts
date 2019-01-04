961a2884c7c81aa5c1343972f7574af8e037cfea
Shelton Suen <sheltonsuen@gmail.com>
2019-01-04 10:13:42 +0800

    Show the hero object.

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,12 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+import Hero from '../hero';
+
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  hero: string = 'Shelton';
+  hero: Hero = {
+    id: 1,
+    name: 'Shelton'
+  };
   constructor() {}
 
   ngOnInit() {}
